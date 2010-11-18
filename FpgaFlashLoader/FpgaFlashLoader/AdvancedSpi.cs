d029a265eb9144f65d1428402b66261a6c04de96
Blake Ramsdell <blaker@gmail.com>
2010-11-17 17:08:44 -0800

    Added manual chip select to AdvancedSpi.

--- FpgaFlashLoader/FpgaFlashLoader/AdvancedSpi.cs
+++ FpgaFlashLoader/FpgaFlashLoader/AdvancedSpi.cs
@@ -7,11 +7,12 @@ namespace FpgaFlashLoader
     class AdvancedSpi
     {
         private SPI spi;
+        private OutputPort chipSelectPort;
 
         public AdvancedSpi(Cpu.Pin chipSelectPin, SPI.SPI_module spiModule)
         {
             SPI.Configuration spiConfig = new SPI.Configuration(
-                chipSelectPin,
+                Cpu.Pin.GPIO_NONE,
                 false,
                 100,
                 100,
@@ -21,16 +22,31 @@ namespace FpgaFlashLoader
                 spiModule
             );
             spi = new SPI(spiConfig);
+            this.chipSelectPort = new OutputPort(chipSelectPin, true);
+        }
+
+        private void ChipSelectActive()
+        {
+            chipSelectPort.Write(false);
+        }
+
+        private void ChipSelectInactive()
+        {
+            chipSelectPort.Write(true);
         }
 
         public void Write(byte[] writeBuffer)
         {
+            ChipSelectActive();
             spi.Write(writeBuffer);
+            ChipSelectInactive();
         }
 
         public void WriteRead(byte[] writeBuffer, byte[] readBuffer)
         {
+            ChipSelectActive();
             spi.WriteRead(writeBuffer, readBuffer);
+            ChipSelectInactive();
         }
     }
 }
