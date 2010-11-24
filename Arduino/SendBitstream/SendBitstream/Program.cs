7c03fc309f313804338365451d0800dc378cf7b9
Blake Ramsdell <blaker@gmail.com>
2010-11-24 03:44:13 -0800

    First cut of serial port PC side.

--- Arduino/SendBitstream/SendBitstream/Program.cs
+++ Arduino/SendBitstream/SendBitstream/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO.Ports;
+using System.Threading;
 
 namespace SendBitstream
 {
@@ -9,6 +7,15 @@ namespace SendBitstream
     {
         static void Main(string[] args)
         {
+            SerialPort arduinoPort = new SerialPort("COM3", 115200);
+            arduinoPort.Open();
+            arduinoPort.DataReceived += delegate(object sender, SerialDataReceivedEventArgs e) { System.Console.Write(arduinoPort.ReadExisting()); };
+            // Honk out 268 bytes, which should cause some data to be received
+            for (int counter = 0; counter < 268; ++counter)
+            {
+                arduinoPort.Write("a");
+            }
+            Thread.Sleep(5000);
         }
     }
 }
