79bde8de58f98c6b6fdb64cc917701b483625a87
Blake Ramsdell <blaker@gmail.com>
2010-11-24 04:28:27 -0800

    Added printing of all of the serial ports.

--- Arduino/SendBitstream/SendBitstream/Program.cs
+++ Arduino/SendBitstream/SendBitstream/Program.cs
@@ -15,11 +15,21 @@ namespace SendBitstream
 
         static void Main(string[] args)
         {
+            PrintSerialPortList();
+
             SerialPort arduinoPort = new SerialPort("COM3", 115200);
             arduinoPort.Open();
             arduinoPort.NewLine = "\r\n";   // Consistent with the Arduino Serial.println method which transmits \r\n
 
             System.Console.WriteLine("That dude said {0} so there", TransmitBufferAndAwaitResponse(arduinoPort, new byte[268], 0, 268));
+        }
+
+        private static void PrintSerialPortList()
+        {
+            foreach (var name in SerialPort.GetPortNames())
+            {
+                System.Console.WriteLine("Found port {0} -- give it a whirl.", name);
+            }
         }
     }
 }
