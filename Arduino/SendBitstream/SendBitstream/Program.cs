942c6787dbd37ba944e8b4196e73168cd4368111
Blake Ramsdell <blaker@gmail.com>
2010-11-24 04:14:32 -0800

    Basic foundation of transmitting a page and getting back a response.

--- Arduino/SendBitstream/SendBitstream/Program.cs
+++ Arduino/SendBitstream/SendBitstream/Program.cs
@@ -7,17 +7,19 @@ namespace SendBitstream
 {
     class Program
     {
+        static string TransmitBufferAndAwaitResponse(SerialPort arduinoPort, byte[] buffer, int offset, int count)
+        {
+            arduinoPort.Write(buffer, offset, count);
+            return arduinoPort.ReadLine();
+        }
+
         static void Main(string[] args)
         {
             SerialPort arduinoPort = new SerialPort("COM3", 115200);
             arduinoPort.Open();
-            arduinoPort.DataReceived += delegate(object sender, SerialDataReceivedEventArgs e) { System.Console.Write(arduinoPort.ReadExisting()); };
-            // Honk out 268 bytes, which should cause some data to be received
-            for (int counter = 0; counter < 268; ++counter)
-            {
-                arduinoPort.Write("a");
-            }
-            Thread.Sleep(5000);
+            arduinoPort.NewLine = "\r\n";   // Consistent with the Arduino Serial.println method which transmits \r\n
+
+            System.Console.WriteLine("That dude said {0} so there", TransmitBufferAndAwaitResponse(arduinoPort, new byte[268], 0, 268));
         }
     }
 }
