982c41b8e022b43fad967e510d23b85f016c0c99
Blake Ramsdell <blaker@gmail.com>
2011-01-09 23:59:27 -0800

    More consistency in constants.

--- FpgaFlashLoader/Prototype.Xilinx/Constants.cs
+++ FpgaFlashLoader/Prototype.Xilinx/Constants.cs
@@ -6,11 +6,13 @@ namespace Prototype.Xilinx
     {
         // http://www.xilinx.com/support/documentation/user_guides/ug333.pdf
 
-        public static readonly int SramPageBufferSize = 264;
         public const byte PadByte = 0xFF;
-        public const int BootloaderStartAddress = 0x000000;
-        public const int UserStartAddress = 0x020000;
-        public const int PageIncrement = 0x000200;
+
+        public static readonly int SramPageBufferSize = 264;
+
+        public static readonly int BootloaderStartAddress = 0x000000;
+        public static readonly int UserStartAddress = 0x020000;
+        public static readonly int PageIncrement = 0x000200;
 
         public static readonly byte StatusRegisterReadyMask = 0x80;
         public static readonly byte StatusRegisterCompareMask = 0x40;
