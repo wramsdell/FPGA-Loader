e1cb35f297349b3804473aa3f495e40a85708b24
Blake Ramsdell <blaker@gmail.com>
2011-01-10 00:16:38 -0800

    Moved ShieldConfiguration to his own place.

--- FpgaFlashLoader/FpgaFlashLoader/Program.cs
+++ FpgaFlashLoader/FpgaFlashLoader/Program.cs
@@ -1,91 +1,13 @@
 ﻿// Copyright (C) Prototype Engineering, LLC. All rights reserved.
 
 using System.Collections;
-using Microsoft.SPOT.Hardware;
 using System.Threading;
+using Microsoft.SPOT.Hardware;
 
 namespace FpgaFlashLoader
 {
     public class Program
     {
-        private static class NetduinoConstants
-        {
-            public const Cpu.Pin GPIO_PIN_D0 = (Cpu.Pin) 27;
-            public const Cpu.Pin GPIO_PIN_D1 = (Cpu.Pin) 28;
-            public const Cpu.Pin GPIO_PIN_D2 = (Cpu.Pin) 0;
-            public const Cpu.Pin GPIO_PIN_D3 = (Cpu.Pin) 1;
-            public const Cpu.Pin GPIO_PIN_D4 = (Cpu.Pin) 12;
-            public const Cpu.Pin ONBOARD_LED = (Cpu.Pin) 55;
-        }
-
-        private static class FezConstants
-        {
-            public const Cpu.Pin LED = (Cpu.Pin) 4;
-            public const Cpu.Pin Di4 = (Cpu.Pin) 19;
-            public const Cpu.Pin Di3 = (Cpu.Pin) 31;
-            public const Cpu.Pin Di2 = (Cpu.Pin) 33;
-        }
-
-        private class ShieldConfiguration
-        {
-            public Cpu.Pin RedLedPin { get; set;  }
-            public Cpu.Pin GreenLedPin { get; set;  }
-            public Cpu.Pin SpiChipSelectPin { get; set;  }
-            public Cpu.Pin OnboardLedPin { get; set; }
-        }
-
-        // Bootlader v1 configuration
-        private static ShieldConfiguration OldNetduinoShieldConfiguration = new ShieldConfiguration
-        {
-            RedLedPin = NetduinoConstants.GPIO_PIN_D1,
-            GreenLedPin = NetduinoConstants.GPIO_PIN_D2,
-            SpiChipSelectPin = NetduinoConstants.GPIO_PIN_D0,
-            OnboardLedPin = NetduinoConstants.ONBOARD_LED
-        };
-
-        // Bootlader v2 configuration
-        private static ShieldConfiguration NetduinoShieldConfiguration = new ShieldConfiguration
-        {
-            RedLedPin = NetduinoConstants.GPIO_PIN_D3,
-            GreenLedPin = NetduinoConstants.GPIO_PIN_D4,
-            SpiChipSelectPin = NetduinoConstants.GPIO_PIN_D2,
-            OnboardLedPin = NetduinoConstants.ONBOARD_LED
-        };
-
-        private static ShieldConfiguration FezShieldConfiguration = new ShieldConfiguration
-        {
-            RedLedPin = FezConstants.Di3,
-            GreenLedPin = FezConstants.Di4,
-            SpiChipSelectPin = FezConstants.Di2,
-            OnboardLedPin = FezConstants.LED
-        };
-
-        private static ShieldConfiguration CurrentShieldConfiguration = null;
-
-        // SetupCurrentShieldConfiguration is a little fragile -- it uses
-        // Microsoft.SPOT.Hardware.SystemInfo.OEMString to try and figure out
-        // which configuration to use. If that string isn't handled, you will
-        // not survive.
-
-        private static void SetupCurrentShieldConfiguration()
-        {
-            var oemString = SystemInfo.OEMString;
-
-            switch (oemString)
-            {
-                case "GHI Electronics, LLC":
-                    CurrentShieldConfiguration = FezShieldConfiguration;
-                    break;
-
-                case "Netduino by Secret Labs LLC":
-                    CurrentShieldConfiguration = NetduinoShieldConfiguration;
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
         public static IEnumerator GetResources()
         {
             yield return Resources.BinaryResources.Bitstream_bin1;
@@ -145,12 +67,10 @@ namespace FpgaFlashLoader
 
         public static void Main()
         {
-            SetupCurrentShieldConfiguration();
-
-            OutputPort onboardLed = new OutputPort(CurrentShieldConfiguration.OnboardLedPin, true);
+            OutputPort onboardLed = new OutputPort(ShieldConfiguration.CurrentConfiguration.OnboardLedPin, true);
 
             SPI.Configuration spiConfig = new SPI.Configuration(
-                CurrentShieldConfiguration.SpiChipSelectPin,
+                ShieldConfiguration.CurrentConfiguration.SpiChipSelectPin,
                 false,
                 100,
                 100,
@@ -164,7 +84,10 @@ namespace FpgaFlashLoader
 
             if (uploader.IsShieldInBootloaderMode())
             {
-                var uploadStatusIndicator = new UploadStatusIndicator(CurrentShieldConfiguration.RedLedPin, CurrentShieldConfiguration.GreenLedPin);
+                var uploadStatusIndicator = new UploadStatusIndicator(
+                    ShieldConfiguration.CurrentConfiguration.RedLedPin,
+                    ShieldConfiguration.CurrentConfiguration.GreenLedPin
+                    );
 
                 try
                 {
---
+++ FpgaFlashLoader/FpgaFlashLoader/ShieldConfiguration.cs
@@ -0,0 +1,92 @@
+// Copyright (C) Prototype Engineering, LLC. All rights reserved.
+
+using Microsoft.SPOT.Hardware;
+
+namespace FpgaFlashLoader
+{
+    class ShieldConfiguration
+    {
+        public Cpu.Pin RedLedPin { get; set;  }
+        public Cpu.Pin GreenLedPin { get; set;  }
+        public Cpu.Pin SpiChipSelectPin { get; set;  }
+        public Cpu.Pin OnboardLedPin { get; set; }
+
+        private static class NetduinoConstants
+        {
+            public const Cpu.Pin GPIO_PIN_D0 = (Cpu.Pin) 27;
+            public const Cpu.Pin GPIO_PIN_D1 = (Cpu.Pin) 28;
+            public const Cpu.Pin GPIO_PIN_D2 = (Cpu.Pin) 0;
+            public const Cpu.Pin GPIO_PIN_D3 = (Cpu.Pin) 1;
+            public const Cpu.Pin GPIO_PIN_D4 = (Cpu.Pin) 12;
+            public const Cpu.Pin ONBOARD_LED = (Cpu.Pin) 55;
+        }
+
+        private static class FezConstants
+        {
+            public const Cpu.Pin LED = (Cpu.Pin) 4;
+            public const Cpu.Pin Di4 = (Cpu.Pin) 19;
+            public const Cpu.Pin Di3 = (Cpu.Pin) 31;
+            public const Cpu.Pin Di2 = (Cpu.Pin) 33;
+        }
+
+        // Bootlader v1 configuration
+        private static ShieldConfiguration OldNetduinoShieldConfiguration = new ShieldConfiguration
+        {
+            RedLedPin = NetduinoConstants.GPIO_PIN_D1,
+            GreenLedPin = NetduinoConstants.GPIO_PIN_D2,
+            SpiChipSelectPin = NetduinoConstants.GPIO_PIN_D0,
+            OnboardLedPin = NetduinoConstants.ONBOARD_LED
+        };
+
+        // Bootlader v2 configuration
+        private static ShieldConfiguration NetduinoShieldConfiguration = new ShieldConfiguration
+        {
+            RedLedPin = NetduinoConstants.GPIO_PIN_D3,
+            GreenLedPin = NetduinoConstants.GPIO_PIN_D4,
+            SpiChipSelectPin = NetduinoConstants.GPIO_PIN_D2,
+            OnboardLedPin = NetduinoConstants.ONBOARD_LED
+        };
+
+        private static ShieldConfiguration FezShieldConfiguration = new ShieldConfiguration
+        {
+            RedLedPin = FezConstants.Di3,
+            GreenLedPin = FezConstants.Di4,
+            SpiChipSelectPin = FezConstants.Di2,
+            OnboardLedPin = FezConstants.LED
+        };
+
+        private static ShieldConfiguration shieldConfiguration;
+
+        // SetupCurrentShieldConfiguration is a little fragile -- it uses
+        // Microsoft.SPOT.Hardware.SystemInfo.OEMString to try and figure out
+        // which configuration to use. If that string isn't handled, you will
+        // not survive.
+
+        public static ShieldConfiguration CurrentConfiguration
+        {
+            get
+            {
+                if (shieldConfiguration == null)
+                {
+                    var oemString = SystemInfo.OEMString;
+
+                    switch (oemString)
+                    {
+                        case "GHI Electronics, LLC":
+                            shieldConfiguration = FezShieldConfiguration;
+                            break;
+
+                        case "Netduino by Secret Labs LLC":
+                            shieldConfiguration = NetduinoShieldConfiguration;
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+
+                return shieldConfiguration;
+            }
+        }
+    }
+}
